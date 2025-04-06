e9e0fe8086c7b6d19880e1f05cf70e6590ffc2dd
karim523 <mostafa.karimzadeh@kpn.com>
2025-04-06 12:36:05 +0200

    Refactored main.py.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from dotenv import load_dotenv
-from utility import ReActAgent
+from utility import Agent
+
 
 def main():
     load_dotenv()
-    agent = ReActAgent()
-    response = agent.say_hello()
+    system_instruction = "You are a helpful assistant."
+    agent = Agent(system=system_instruction)
+    response = agent("Hello world")
     print("GPT Response:", response)
+
 
 if __name__ == "__main__":
     main()
