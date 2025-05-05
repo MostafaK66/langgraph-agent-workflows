f3e04fd893d43346676b45a4968d81d152538075
karim523 <mostafa.karimzadeh@kpn.com>
2025-05-05 10:52:44 +0200

    Refactored with TavilySearchResults.

--- agent_tools.py
+++ agent_tools.py
@@ -1,23 +1,10 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+
 class AgentTools:
     def __init__(self):
-        pass
-
-    def calculate(self, what):
-        return eval(what)
-
-    def average_dog_weight(self, name):
-        name = name.strip().lower()
-        if "scottish terrier" in name:
-            return "Scottish Terriers average 20 lbs"
-        elif "border collie" in name:
-            return "A Border Collie's average weight is 37 lbs"
-        elif "toy poodle" in name:
-            return "A Toy Poodle's average weight is 7 lbs"
-        else:
-            return "An average dog weighs 50 lbs"
+        self.tool = TavilySearchResults(max_results=4)
 
     def get_known_actions(self):
-        return {
-            "calculate": self.calculate,
-            "average_dog_weight": self.average_dog_weight
-        }
+        return [self.tool]
+
