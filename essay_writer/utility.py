f112fa43b162ec7ce80aa347e0dc9fdd748bcd0e
karim523 <mostafa.karimzadeh@kpn.com>
2025-06-01 13:17:23 +0200

    Added the necessary TavilyClient to utility.py.

--- essay_writer/utility.py
+++ essay_writer/utility.py
@@ -1,8 +1,15 @@
 from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+import os
 
 class EssayWriterAgent:
     def __init__(self):
         self.model = self._initialize_model()
+        self.tavily = self._initialize_tavily_client()
 
     def _initialize_model(self):
         return ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+    def _initialize_tavily_client(self):
+        return TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
+
