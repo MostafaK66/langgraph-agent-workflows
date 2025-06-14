6cba613e5ff9be82e7b79a7ff907ac9340047e0a
karim523 <mostafa.karimzadeh@kpn.com>
2025-06-14 11:39:38 +0200

    Code cleaning on graph_builder.py

--- essay_writer/graph_builder.py
+++ essay_writer/graph_builder.py
@@ -12,40 +12,32 @@ class EssayWriterGraph:
         :param agent: an instance of EssayWriterAgent, whose node methods we register
         :param checkpointer: optional LangGraph saver; if None, uses an in‐memory SQLite saver
         """
-        # 1) Create a builder over your AgentState TypedDict
         builder = StateGraph(AgentState)
 
-        # 2) Register nodes, binding to this agent’s methods
         builder.add_node("planner", agent.plan_node)
         builder.add_node("research_plan", agent.research_plan_node)
         builder.add_node("generate", agent.generation_node)
         builder.add_node("reflect", agent.reflection_node)
         builder.add_node("research_critique", agent.research_critique_node)
 
-        # 3) Entry point of the graph
         builder.set_entry_point("planner")
 
-        # 4) Linear edges for the “happy path”
         builder.add_edge("planner", "research_plan")
         builder.add_edge("research_plan", "generate")
 
-        # 5) After “generate” we decide whether to loop or finish
         builder.add_conditional_edges(
             "generate",
-            agent.should_continue,  # returns either END or "reflect"
+            agent.should_continue,
             {END: END,
              "reflect": "reflect"}
         )
 
-        # 6) The reflection sub‐loop
         builder.add_edge("reflect", "research_critique")
         builder.add_edge("research_critique", "generate")
 
-        # 7) Set up the checkpointer (defaults to in‐memory SQLite)
         if checkpointer is None:
             memory = Persistence.synchronous(":memory:")
         else:
             memory = checkpointer
 
-        # 8) Compile into an executable graph
         self.graph = builder.compile(checkpointer=memory)
